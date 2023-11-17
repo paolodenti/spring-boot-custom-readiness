91cb4c673f0b8f3c858dc80e0b2ffeac21579575
Paolo Denti <paolo.denti@gmail.com>
2023-11-16 22:25:11 -0800

    refactoring

--- src/main/java/com/github/paolodenti/readiness/service/StartupSimulatorService.java
+++ src/main/java/com/github/paolodenti/readiness/service/StartupSimulatorService.java
@@ -1,30 +1,33 @@
 package com.github.paolodenti.readiness.service;
 
 import com.github.paolodenti.readiness.probe.CustomReadinessIndicator;
-import jakarta.annotation.PostConstruct;
 import java.util.concurrent.TimeUnit;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
-public class StartupSimulatorService {
+public class StartupSimulatorService implements ApplicationListener<ApplicationReadyEvent> {
 
     private final CustomReadinessIndicator customReadinessIndicator;
 
     /**
      * Slow startup simulation, setting readiness to up after 15 seconds.
      */
-    @PostConstruct
-    private void postConstruct() {
+    @Override
+    public void onApplicationEvent(@NonNull ApplicationReadyEvent event) {
 
+        log.info("Application is fully started, simulating slow startup");
         new Thread(() -> {
             try {
                 TimeUnit.SECONDS.sleep(15L);
 
-                log.info("Readiness going up");
+                log.info("Startup has completed, setting readiness to up");
                 customReadinessIndicator.setReadyStatus(true);
             } catch (InterruptedException ex) {
                 Thread.currentThread().interrupt();
